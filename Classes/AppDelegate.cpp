b5a8664e596f983fbc8d136ccec560260e8fce20
SShayashi <SShayashi0208@gmail.com>
2016-01-24 16:53:09 +0900

    デバッグ用の表示を消去

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -44,12 +44,6 @@ bool AppDelegate::applicationDidFinishLaunching() {
 
     director->getOpenGLView()->setDesignResolutionSize(640, 1136, ResolutionPolicy::SHOW_ALL);
 
-    // turn on display FPS
-    director->setDisplayStats(true);
-
-    // set FPS. the default value is 1.0/60 if you don't call this
-    director->setAnimationInterval(1.0 / 60);
-
     FileUtils::getInstance()->addSearchPath("res");
 
     //ヘルパークラスを初期化
